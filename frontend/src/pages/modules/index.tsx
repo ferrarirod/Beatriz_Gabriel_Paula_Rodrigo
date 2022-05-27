8a1f0877ab9b6ad8106cd1e581e8907fb2147b39
PaulaRRP <paularincorp@gmail.com>
2022-05-26 22:54:00 -0300

    adiconando arquivos dos modulos e migration da tabela de modulos

---
+++ frontend/src/pages/modules/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export function ListModulesPage() {
+
+    return (
+        <div>
+            <h1>Lista de Módulos</h1>
+            <Link to="/home" >Próxima</Link>
+        </div>
+    )
+}
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { ListUsersPage } from "../pages/users";
+import { ListModulesPage } from "../pages/modules"
 import { Private } from "../templates/Private";
 
 export function AppRoutes() {
@@ -7,6 +8,7 @@ export function AppRoutes() {
     <Private>
       <Routes>
         <Route path="/" element={<ListUsersPage />} />
+        <Route path="/modules" element={<ListModulesPage />} />
       </Routes>
     </Private>
   );
